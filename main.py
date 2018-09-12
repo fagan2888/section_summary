f792268527a2b513b0480543b4ddd2283e157408
Kavi Gupta <kavi@globalmbox.com>
2018-09-12 00:31:14 -0700

    allow for comma separated emails

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import tempfile
 from user_summary import get_scores_for_all_emails
 from visualize import visualize
 
-all_scores = get_scores_for_all_emails(argv[1], int(argv[2]), open(argv[3]).read().split())
+all_scores = get_scores_for_all_emails(argv[1], int(argv[2]), open(argv[3]).read().replace(",", " ").split())
 f = tempfile.NamedTemporaryFile('w', suffix='.html', delete=False)
 html, csv = visualize(all_scores)
 f.write(html)
